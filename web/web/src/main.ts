1b337afe213714620e2366306793e56f121d8589
null122 <null122@users.noreply.github.com>
2022-01-21 21:37:04 +0800

    重构

--- web/web/src/main.ts
+++ web/web/src/main.ts
@@ -1,10 +1,12 @@
+import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import router from './router'
 
 loadFonts()
 
-createApp(App)
+createApp(App).use(router)
   .use(vuetify)
   .mount('#app')
