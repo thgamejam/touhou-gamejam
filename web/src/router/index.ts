5a17a54b76e4998b2f3bc0536173538b638cb347
null122 <null122@users.noreply.github.com>
2022-01-24 17:39:58 +0800

    傻逼antd，狗都不用

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,12 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-// import Home from '../views/Home.vue'
-import Main from '../views/Main.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
-    name: 'Main',
-    component: Main
+    name: 'Home',
+    component: Home
   },
   {
     path: '/about',
