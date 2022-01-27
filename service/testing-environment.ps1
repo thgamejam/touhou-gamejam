18eb78ccec37ea3c06e61a4c4b4a781d61d58a71
strluck <s@strluck.com>
2022-01-28 04:03:46 +0800

    更换consul

--- service/testing-environment.ps1
+++ service/testing-environment.ps1
@@ -1,4 +1,4 @@
-docker run -d --name nacos -p 8848:8848 -p 9848:9848 -e MODE=standalone nacos/nacos-server:2.0.2
+docker run -d --name consul -p 8500:8500 -p 8600:8600 -e CONSUL_BIND_INTERFACE=eth0 consul:1.11.2
 docker run -d --name mariadb -p 3306:3306 --env MARIADB_ROOT_PASSWORD=123456 mariadb:10.6.5
 docker run -d --name redis -p 6379:6379 redis:6.2
 docker run -d --name minio -p 9000:9000 -p 9001:9001 minio/minio server /data --console-address ":9001"
