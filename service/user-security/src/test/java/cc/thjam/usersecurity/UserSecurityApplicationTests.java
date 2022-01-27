5dacb67a40dac51eed55ca2e5993459b4b03eab2
strluck <s@strluck.com>
2022-01-28 04:04:12 +0800

    创建用户安全服务

---
+++ service/user-security/src/test/java/cc/thjam/usersecurity/UserSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.thjam.usersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
