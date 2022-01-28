c764115a8c1c4e1e4a153c6f5b541807a0a3223c
strluck <s@strluck.com>
2022-01-29 00:59:10 +0800

    创建样例

---
+++ service/examples/src/test/java/cc/thjam/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.thjam.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
