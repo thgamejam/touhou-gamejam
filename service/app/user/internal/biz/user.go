9e4ad4579b7bb38822b372e71e92e4cbbf2a92a5
strluck <s@strluck.com>
2022-03-01 11:35:23 +0800

    修复 err is shadowed during return
    就tm离谱这个错误

--- service/app/user/internal/biz/user.go
+++ service/app/user/internal/biz/user.go
@@ -55,31 +55,31 @@ func (uc *UserUseCase) Create(ctx context.Context, u *User) error {
 // VerifyPassword 验证密码
 // 注意:	用户名只允许"-"和"_"这两个符号
 //		电话号码使用 国际区号+电话号 的格式, 86+18100000000
-func (uc *UserUseCase) VerifyPassword(ctx context.Context, a *Account) (b bool, err error) {
+func (uc *UserUseCase) VerifyPassword(ctx context.Context, a *Account) (ok bool, err error) {
 	var user *User
 	switch a.Format {
 	case v1.AccountFormat_ID:
 		id, err := strconv.ParseUint(a.Username, 10, 64)
 		if err != nil {
-			return
+			return false, err
 		}
 		user, err = uc.repo.GetUserByUserID(ctx, id)
 		if err != nil {
-			return
+			return false, err
 		}
 		break
 
 	case v1.AccountFormat_USERNAME:
 		user, err = uc.repo.GetUserByUsername(ctx, a.Username)
 		if err != nil {
-			return
+			return false, err
 		}
 		break
 
 	case v1.AccountFormat_EMAIL:
 		user, err = uc.repo.GetUserByEmail(ctx, a.Username)
 		if err != nil {
-			return
+			return false, err
 		}
 		break
 
@@ -87,21 +87,21 @@ func (uc *UserUseCase) VerifyPassword(ctx context.Context, a *Account) (b bool, 
 		// 拆分手机号, 86+18100000000
 		phone := strings.Split(a.Username, "+")
 		if len(phone) < 2 {
-			return false, nil
+			return false, err
 		}
 		code, err := strconv.ParseUint(phone[0], 10, 16)
 		if err != nil {
-			return
+			return false, err
 		}
 		tel := &TelPhone{TelCode: uint16(code), Phone: phone[1]}
 		user, err = uc.repo.GetUserByPhone(ctx, tel)
 		if err != nil {
-			return
+			return false, err
 		}
 		break
 
 	default:
-		return false, nil
+		return false, err
 	}
 
 	return uc.repo.VerifyPassword(ctx, user)
