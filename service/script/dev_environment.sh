39d5fe9a50801df4c85ea3922f60f85a4f570848
Strluck <s@strluck.com>
2022-03-12 20:24:03 +0800

    创建docker网络

--- service/script/dev_environment.sh
+++ service/script/dev_environment.sh
@@ -4,8 +4,16 @@ registry="./script/docker/dev/registry/consul-compose.yaml"
 database="./script/docker/dev/database/database-compose.yaml"
 queue="./script/docker/dev/queue/rocktemq-compose.yaml"
 
-docker_compose="docker-compose.exe"
 # 判断Docker的环境
+docker="docker.exe"
+docker_path=$( command -v docker.exe )
+if [ -z "$docker_path" ]
+then
+   docker_path=$( command -v docker )
+   docker="docker"
+fi
+
+docker_compose="docker-compose.exe"
 docker_compose_path=$( command -v docker-compose.exe )
 if [ -z "$docker_compose_path" ]
 then
@@ -13,7 +21,15 @@ then
    docker_compose="docker-compose"
 fi
 
-echo "docker path: $docker_compose_path"
+echo "docker path: $docker_path"
+echo "docker-compose path: $docker_compose_path"
+
+# 创建网络
+network_name="th_bridge"
+filter_name=$( $docker network ls | grep $network_name | awk '{ print $2 }' )
+if [ "$filter_name" == "" ]; then
+    $docker network create --subnet=172.84.0.0/24 --gateway=172.84.0.1 th_bridge
+fi
 
 $docker_compose -f $registry up -d
 $docker_compose -f $database up -d
