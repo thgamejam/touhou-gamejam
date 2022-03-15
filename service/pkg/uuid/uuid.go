12cc85846219941ca957423d3e2c33cae25e4818
Strluck <s@strluck.com>
2022-03-15 23:53:59 +0800

    修改排序uuid

--- service/pkg/uuid/uuid.go
+++ service/pkg/uuid/uuid.go
@@ -1,9 +1,9 @@
 package uuid
 
 import (
-	"database/sql/driver"
-	"encoding/hex"
-	"github.com/google/uuid"
+    "database/sql/driver"
+    "encoding/hex"
+    "github.com/google/uuid"
 )
 
 // UUID 封装的google uuid
@@ -11,88 +11,85 @@ type UUID uuid.UUID
 
 // New 生成UUIDv4
 func New() UUID {
-	return UUID(uuid.New())
+    return UUID(uuid.New())
 }
 
 // NewUUID1 生成UUIDv1
 func NewUUID1() (u1 UUID, err error) {
-	u, err := uuid.NewUUID()
-	if err != nil {
-		return
-	}
-	u1 = UUID(u)
-	return
+    u, err := uuid.NewUUID()
+    if err != nil {
+        return
+    }
+    u1 = UUID(u)
+    return
 }
 
 // NewOrderedUUID 生成有序UUID
 func NewOrderedUUID() (ou UUID, err error) {
-	ou, err = NewUUID1()
-	if err != nil {
-		return
-	}
+    ou, err = NewUUID1()
+    if err != nil {
+        return
+    }
 
-	// 重新排序uuid
-	tmp := make([]byte, 4, 4)
-	tmp[0] = ou[6]
-	tmp[1] = ou[7]
-	tmp[2] = ou[4]
-	tmp[3] = ou[5]
+    // 重新排序uuid
+    ou[4], ou[6] = ou[6], ou[4]
+    ou[5], ou[7] = ou[7], ou[5]
+    ou[0], ou[4] = ou[4], ou[0]
+    ou[1], ou[5] = ou[5], ou[1]
+    ou[2], ou[6] = ou[6], ou[2]
+    ou[3], ou[7] = ou[7], ou[3]
 
-	for i := 0; i < 4; i++ {
-		ou[i+4] = ou[i]
-		ou[i] = tmp[i]
-	}
-	return
+    return
 }
 
 // Scan scan uuid
 func (b *UUID) Scan(value interface{}) (err error) {
-	bytes, _ := value.([]byte)
-	parseByte, err := uuid.FromBytes(bytes)
-	if err != nil {
-		return
-	}
-	*b = UUID(parseByte)
-	return
+    bytes, _ := value.([]byte)
+    parseByte, err := uuid.FromBytes(bytes)
+    if err != nil {
+        return
+    }
+    *b = UUID(parseByte)
+    return
 }
 
 // Value get uuid value
 func (b UUID) Value() (driver.Value, error) {
-	return uuid.UUID(b).MarshalBinary()
+    return uuid.UUID(b).MarshalBinary()
 }
 
 // Parse 将字符串解析为 UUID
 func Parse(s string) (UUID, error) {
-	id, err := uuid.Parse(s)
-	return UUID(id), err
+    id, err := uuid.Parse(s)
+    return UUID(id), err
 }
 
 // String 返回uuid字符串
 func (b UUID) String() string {
-	return hex.EncodeToString(b[:])
+    return hex.EncodeToString(b[:])
 }
 
 // Format 返回uuid字符串, xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 func (b UUID) Format() string {
-	return uuid.UUID(b).String()
+    return uuid.UUID(b).String()
 }
 
 // GormDataType 数据库的数据类型
 func (b UUID) GormDataType() string {
-	return "binary(16)"
+    return "binary(16)"
 }
 
 func (b UUID) MarshalJSON() ([]byte, error) {
-	s := uuid.UUID(b)
-	str := "\"" + s.String() + "\""
-	return []byte(str), nil
+    s := uuid.UUID(b)
+    str := "\"" + s.String() + "\""
+    return []byte(str), nil
 }
 
 func (b *UUID) UnmarshalJSON(by []byte) (err error) {
-	s, err := uuid.ParseBytes(by)
-	if err != nil {
-		return
-	}
-	*b = UUID(s)
-	return
+    s, err := uuid.ParseBytes(by)
+    if err != nil {
+        return
+    }
+    *b = UUID(s)
+    return
 }
--- service/pkg/uuid/uuid_test.go
+++ service/pkg/uuid/uuid_test.go
@@ -1,43 +1,52 @@
 package uuid
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+    "github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestUUID_New(t *testing.T) {
-	uuid := New()
-	t.Logf("TestUUID_New:  New log.uuid:=%v\n", uuid.Format())
+    uuid := New()
+    t.Logf("log.uuid:=%v\n", uuid.Format())
 }
 
 func TestUUID_NewUUID1(t *testing.T) {
-	uuid, err := NewUUID1()
-	assert.NoError(t, err)
-	t.Logf("TestUUID_NewUUID1:  NewUUID1 log.uuid:=%v\n", uuid.Format())
+    uuid, err := NewUUID1()
+    assert.NoError(t, err)
+    t.Logf("log.uuid:=%v\n", uuid.Format())
 }
 
 func TestUUID_NewOrderedUUID(t *testing.T) {
-	uuid, err := NewOrderedUUID()
-	assert.NoError(t, err)
-	t.Logf("TestUUID_NewOrderedUUID:  NewOrderedUUID log.uuid:=%v\n", uuid.Format())
+    uuid, err := NewOrderedUUID()
+    assert.NoError(t, err)
+    t.Logf("log.uuid:=%v\n", uuid.Format())
 }
 
 func TestUUID_Parse(t *testing.T) {
-	uuid := New()
-	uuid1, err := NewUUID1()
-	orderedUUID, err := NewOrderedUUID()
-	assert.NoError(t, err)
+    uuid := New()
+    uuid1, err := NewUUID1()
+    orderedUUID, err := NewOrderedUUID()
+    assert.NoError(t, err)
 
-	var parseUUID UUID
-	parseUUID, err = Parse(uuid.String())
-	assert.NoError(t, err)
-	t.Logf("TestUUID_Parse:  Parse-uuid log.uuid:=%v\n", parseUUID.Format())
+    var parseUUID UUID
+    parseUUID, err = Parse(uuid.String())
+    assert.NoError(t, err)
+    t.Logf("log.uuid:=%v\n", parseUUID.Format())
 
-	parseUUID, err = Parse(uuid1.String())
-	assert.NoError(t, err)
-	t.Logf("TestUUID_Parse:  Parse-uuid-1 log.uuid:=%v\n", parseUUID.Format())
+    parseUUID, err = Parse(uuid1.String())
+    assert.NoError(t, err)
+    t.Logf("log.uuid:=%v\n", parseUUID.Format())
 
-	parseUUID, err = Parse(orderedUUID.String())
-	assert.NoError(t, err)
-	t.Logf("TestUUID_Parse:  Parse-uuid-ordered log.uuid:=%v\n", parseUUID.Format())
+    parseUUID, err = Parse(orderedUUID.String())
+    assert.NoError(t, err)
+    t.Logf("log.uuid:=%v\n", parseUUID.Format())
+}
+
+func BenchmarkUUID_NewOrderedUUID(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        _, err := NewOrderedUUID()
+        if err != nil {
+            b.Errorf("failed!err:=%v", err)
+        }
+    }
 }
