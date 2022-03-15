ae4e71a2aa18fca074745c93224ac6c675c88e45
Strluck <s@strluck.com>
2022-03-15 23:55:37 +0800

    重新编配服务，增加绑定User接口

--- service/app/account/internal/biz/biz.go
+++ service/app/account/internal/biz/biz.go
@@ -1,6 +1,51 @@
 package biz
 
-import "github.com/google/wire"
+import (
+    "context"
+    "github.com/go-kratos/kratos/v2/log"
+    "github.com/google/wire"
+)
 
 // ProviderSet is biz providers.
 var ProviderSet = wire.NewSet(NewAccountUseCase)
+
+type AccountRepo interface {
+    // CreateEMailAccount 创建邮箱账户
+    // 返回创造账户的id
+    CreateEMailAccount(context.Context, *Account) (uint64, error)
+
+    // GetAccountByID 通过用户ID获取账户
+    GetAccountByID(context.Context, uint64) (*Account, error)
+    // GetAccountByEMail 通过用户邮箱获取账户
+    GetAccountByEMail(context.Context, string) (*Account, error)
+    // GetAccountByPhone 通过用户手机号获取账户
+    GetAccountByPhone(context.Context, *TelPhone) (*Account, error)
+
+    // ExistAccountEMail 是否存在邮箱
+    ExistAccountEMail(context.Context, string) (bool, error)
+
+    // GetPublicKey 获取公钥
+    // 传入与公钥成对的密钥的md5-16哈希摘要
+    GetPublicKey(context.Context, string) (*PublicKey, error)
+    // GetRandomlyPublicKey 获取任意的一个公钥
+    GetRandomlyPublicKey(context.Context) (*PublicKey, error)
+
+    // GetPrivateKey 获取密钥
+    // 传入密钥的md5-16哈希摘要
+    GetPrivateKey(context.Context, string) (*PrivateKey, error)
+
+    // UpdateAccount 更新账户
+    UpdateAccount(context.Context, *Account) error
+
+    // BindUser 绑定用户
+    BindUser(context.Context, uint64, uint64) error
+}
+
+type AccountUseCase struct {
+    repo AccountRepo
+    log  *log.Helper
+}
+
+func NewAccountUseCase(repo AccountRepo, logger log.Logger) *AccountUseCase {
+    return &AccountUseCase{repo: repo, log: log.NewHelper(logger)}
+}
---
+++ service/sql/account/account.sql
@@ -0,0 +1,18 @@
+use touhou_gamejam;
+
+create table account (
+    id          bigint unsigned     auto_increment  primary key,
+    tel_code    smallint unsigned   not null    default 0   comment '国际电话区号',
+    phone       char(11)            not null    default ''  comment '电话号',
+    email       char(32)            not null    default ''  comment '邮箱',
+    user_id     bigint unsigned     not null    default 0   comment '用户表id',
+    uuid        binary(16)          not null                comment 'uuid',
+    password    char(40)            not null                comment '密码哈希值',
+    status      tinyint unsigned    not null    default 0   comment '账号状态',
+    ctime       datetime            not null                comment '创建时间',
+    mtime       datetime            not null                comment '修改时间'
+);
+
+create index idx_account_tel_code_and_phone on account (tel_code, phone);
+
+create unique index idx_account_email ON account (email);
