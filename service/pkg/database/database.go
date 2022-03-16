924df8bc535b0a94c9c9859f6da2db24fd23358b
Strluck <s@strluck.com>
2022-03-17 00:42:23 +0800

    修复database.Model无法自动更新CTime与MTime的bug

--- service/pkg/database/database.go
+++ service/pkg/database/database.go
@@ -1,54 +1,54 @@
 package database
 
 import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"service/pkg/conf"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+    "service/pkg/conf"
 )
 
 // NewDataBase 初始化数据库
 func NewDataBase(c *conf.Service) (*gorm.DB, error) {
-	db, err := gorm.Open(
-		mysql.Open(c.Data.Database.Source),
-		&gorm.Config{
-			//Logger: , // TODO 绑定 Log 未完成
-		})
-	if err != nil {
-		return nil, err
-	}
+    db, err := gorm.Open(
+        mysql.Open(c.Data.Database.Source),
+        &gorm.Config{
+            //Logger: , // TODO 绑定 Log 未完成
+        })
+    if err != nil {
+        return nil, err
+    }
 
-	selDb, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
-	// 设置连接池
-	// 空闲
-	selDb.SetMaxIdleConns(int(c.Data.Database.MaxIdleConn))
-	// 打开
-	selDb.SetMaxOpenConns(int(c.Data.Database.MaxOpenConn))
-	// 超时 time.Second * 30
-	selDb.SetConnMaxLifetime(c.Data.Database.ConnMaxLifetime.AsDuration())
+    selDb, err := db.DB()
+    if err != nil {
+        return nil, err
+    }
+    // 设置连接池
+    // 空闲
+    selDb.SetMaxIdleConns(int(c.Data.Database.MaxIdleConn))
+    // 打开
+    selDb.SetMaxOpenConns(int(c.Data.Database.MaxOpenConn))
+    // 超时 time.Second * 30
+    selDb.SetConnMaxLifetime(c.Data.Database.ConnMaxLifetime.AsDuration())
 
-	err = registerCallbacks(db)
-	if err != nil {
-		return nil, err
-	}
+    err = registerCallbacks(db)
+    if err != nil {
+        return nil, err
+    }
 
-	return db, nil
+    return db, nil
 }
 
 func registerCallbacks(db *gorm.DB) (err error) {
-	err = db.Callback().Create().Replace("gorm:create_time_stamp", createCallback)
-	if err != nil {
-		return err
-	}
-	err = db.Callback().Update().Replace("gorm:update_time_stamp", updateCallback)
-	if err != nil {
-		return err
-	}
-	err = db.Callback().Delete().Replace("gorm:delete_time_stamp", deleteCallback)
-	if err != nil {
-		return err
-	}
-	return err
+    err = db.Callback().Create().Before("gorm:create").Replace("gorm:create_time_stamp", createCallback)
+    if err != nil {
+        return err
+    }
+    err = db.Callback().Update().Before("gorm:update").Replace("gorm:update_time_stamp", updateCallback)
+    if err != nil {
+        return err
+    }
+    err = db.Callback().Delete().Before("gorm:delete").Replace("gorm:delete_time_stamp", deleteCallback)
+    if err != nil {
+        return err
+    }
+    return err
 }
--- service/pkg/database/model.go
+++ service/pkg/database/model.go
@@ -1,46 +1,46 @@
 package database
 
 import (
-	"gorm.io/gorm"
-	"time"
+    "gorm.io/gorm"
+    "time"
 )
 
 type Model struct {
-	ID    uint64    `json:"id" gorm:"column:id; primaryKey"`
-	Ctime time.Time `json:"ctime" gorm:"column:ctime"` // 状态最后一次更改
-	Mtime time.Time `json:"-" gorm:"column:mtime"`     // 数据最后一次修改
+    ID    uint64    `json:"id" gorm:"column:id; primaryKey"`
+    Ctime time.Time `json:"ctime" gorm:"column:ctime"` // 状态最后一次更改
+    Mtime time.Time `json:"-" gorm:"column:mtime"`     // 数据最后一次修改
 }
 
 func createCallback(db *gorm.DB) {
-	if db.Statement.Schema != nil {
-		nowTime := time.Now()
-		createTimeField := db.Statement.Schema.LookUpField("ctime")
-		if createTimeField != nil {
-			_ = createTimeField.Set(db.Statement.ReflectValue, nowTime)
-		}
-		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
-		if modifyTimeField != nil {
-			_ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
-		}
-	}
+    if db.Statement.Schema != nil {
+        nowTime := time.Now()
+        createTimeField := db.Statement.Schema.LookUpField("ctime")
+        if createTimeField != nil {
+            _ = createTimeField.Set(db.Statement.ReflectValue, nowTime)
+        }
+        modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+        if modifyTimeField != nil {
+            _ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
+        }
+    }
 }
 
 func updateCallback(db *gorm.DB) {
-	if db.Statement.Schema != nil {
-		nowTime := time.Now()
-		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
-		if modifyTimeField != nil {
-			_ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
-		}
-	}
+    if db.Statement.Schema != nil {
+        nowTime := time.Now()
+        modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+        if modifyTimeField != nil {
+            _ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
+        }
+    }
 }
 
 func deleteCallback(db *gorm.DB) {
-	if db.Statement.Schema != nil {
-		nowTime := time.Now()
-		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
-		if modifyTimeField != nil {
-			_ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
-		}
-	}
+    if db.Statement.Schema != nil {
+        nowTime := time.Now()
+        modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+        if modifyTimeField != nil {
+            _ = modifyTimeField.Set(db.Statement.ReflectValue, nowTime)
+        }
+    }
 }
