b0ae1a1bc98c7f27740f72ad64cb1e6f6f781a64
Strluck <s@strluck.com>
2022-03-17 00:43:40 +0800

    增加ECC固定字符串密钥对加密解密测试

--- service/pkg/crypto/ecc/ecc.go
+++ service/pkg/crypto/ecc/ecc.go
@@ -1,13 +1,13 @@
 package ecc
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"github.com/ethereum/go-ethereum/crypto/ecies"
+    "crypto/ecdsa"
+    "crypto/elliptic"
+    "crypto/rand"
+    "crypto/x509"
+    "encoding/base64"
+    "encoding/pem"
+    "github.com/ethereum/go-ethereum/crypto/ecies"
 )
 
 // 秘钥加密签名，公钥解密签名
@@ -16,95 +16,95 @@ import (
 
 // GenerateKey 生成ECC公钥/秘钥对
 func GenerateKey() (privateKeyStr string, publicKeyStr string, err error) {
-	// 生成秘钥
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return "", "", err
-	}
+    // 生成秘钥
+    privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+    if err != nil {
+        return "", "", err
+    }
 
-	// x509编码
-	eccPrivateKey, err := x509.MarshalECPrivateKey(privateKey)
-	if err != nil {
-		return "", "", err
-	}
+    // x509编码
+    eccPrivateKey, err := x509.MarshalECPrivateKey(privateKey)
+    if err != nil {
+        return "", "", err
+    }
 
-	// pem编码
-	privateBlock := pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: eccPrivateKey,
-	}
-	privateKeyStr = string(pem.EncodeToMemory(&privateBlock))
+    // pem编码
+    privateBlock := pem.Block{
+        Type:  "PRIVATE KEY",
+        Bytes: eccPrivateKey,
+    }
+    privateKeyStr = string(pem.EncodeToMemory(&privateBlock))
 
-	// 获取公钥
-	publicKey := privateKey.PublicKey
-	// x509编码
-	eccPublicKey, err := x509.MarshalPKIXPublicKey(&publicKey)
-	if err != nil {
-		return "", "", err
-	}
+    // 获取公钥
+    publicKey := privateKey.PublicKey
+    // x509编码
+    eccPublicKey, err := x509.MarshalPKIXPublicKey(&publicKey)
+    if err != nil {
+        return "", "", err
+    }
 
-	// pem编码
-	block := pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: eccPublicKey,
-	}
-	publicKeyStr = string(pem.EncodeToMemory(&block))
+    // pem编码
+    block := pem.Block{
+        Type:  "PUBLIC KEY",
+        Bytes: eccPublicKey,
+    }
+    publicKeyStr = string(pem.EncodeToMemory(&block))
 
-	return
+    return
 }
 
 // ParsePrivateKey 解码秘钥
 func ParsePrivateKey(data string) (privateKey *ecdsa.PrivateKey, err error) {
-	// pem解码
-	block, _ := pem.Decode([]byte(data))
-	// x509解码
-	privateKey, err = x509.ParseECPrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	return
+    // pem解码
+    block, _ := pem.Decode([]byte(data))
+    // x509解码
+    privateKey, err = x509.ParseECPrivateKey(block.Bytes)
+    if err != nil {
+        return nil, err
+    }
+    return
 }
 
 // ParsePublicKey 解码共钥
 func ParsePublicKey(data string) (publicKey *ecdsa.PublicKey, err error) {
-	// pem解密
-	block, _ := pem.Decode([]byte(data))
-	// x509解密
-	publicInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	publicKey = publicInterface.(*ecdsa.PublicKey)
-	return
+    // pem解密
+    block, _ := pem.Decode([]byte(data))
+    // x509解密
+    publicInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+    if err != nil {
+        return nil, err
+    }
+    publicKey = publicInterface.(*ecdsa.PublicKey)
+    return
 }
 
 // Encrypt ECC加密
 func Encrypt(publicKey *ecdsa.PublicKey, pt []byte) (string, error) {
-	// ecdsa key 转换 ecies key
-	eciesPublicKey := ecies.ImportECDSAPublic(publicKey)
-	// 加密
-	ct, err := ecies.Encrypt(rand.Reader, eciesPublicKey, pt, nil, nil)
-	if err != nil {
-		return "", err
-	}
-	// base64编码
-	ctB64 := base64.StdEncoding.EncodeToString(ct)
-	return ctB64, err
+    // ecdsa key 转换 ecies key
+    eciesPublicKey := ecies.ImportECDSAPublic(publicKey)
+    // 加密
+    ct, err := ecies.Encrypt(rand.Reader, eciesPublicKey, pt, nil, nil)
+    if err != nil {
+        return "", err
+    }
+    // base64编码
+    ctB64 := base64.StdEncoding.EncodeToString(ct)
+    return ctB64, err
 }
 
 // Decrypt ECC解密
 func Decrypt(privateKey *ecdsa.PrivateKey, ctB64 string) ([]byte, error) {
-	// base64解码
-	ct, err := base64.StdEncoding.DecodeString(ctB64)
-	if err != nil {
-		return nil, err
-	}
-	// ecdsa key 转换 ecies key
-	eciesPrivateKey := ecies.ImportECDSA(privateKey)
-	// 解密
-	m, err := eciesPrivateKey.Decrypt(ct, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-	return m, err
+    // base64解码
+    ct, err := base64.StdEncoding.DecodeString(ctB64)
+    if err != nil {
+        return nil, err
+    }
+    // ecdsa key 转换 ecies key
+    eciesPrivateKey := ecies.ImportECDSA(privateKey)
+    // 解密
+    m, err := eciesPrivateKey.Decrypt(ct, nil, nil)
+    if err != nil {
+        return nil, err
+    }
+    return m, err
 }
--- service/pkg/crypto/ecc/ecc_test.go
+++ service/pkg/crypto/ecc/ecc_test.go
@@ -1,104 +1,113 @@
 package ecc
 
 import (
-	"github.com/stretchr/testify/assert"
-	"service/pkg/uuid"
-	"testing"
+    "github.com/stretchr/testify/assert"
+    "service/pkg/uuid"
+    "testing"
+)
+
+const (
+    PrivateKeyStr = "-----BEGIN PRIVATE KEY-----\nMHcCAQEEIIML/IS84s2F47ECIahNMfVn5Xt19qhmmc/pluc2bFqjoAoGCCqGSM49\nAwEHoUQDQgAExPKMCcthv+yG/PsUeKmyOHKk9Dh+4gHifRSABhvLTS+QN8DGsTiQ\nF6TluqXVigzdUoqYc4mHzw4MPG5DqtzXrQ==\n-----END PRIVATE KEY-----\n"
+    PublicKeyStr  = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExPKMCcthv+yG/PsUeKmyOHKk9Dh+\n4gHifRSABhvLTS+QN8DGsTiQF6TluqXVigzdUoqYc4mHzw4MPG5DqtzXrQ==\n-----END PUBLIC KEY-----\n"
 )
 
 func TestECC_GenerateKey(t *testing.T) {
-	privateKeyStr, publicKeyStr, err := GenerateKey()
-	assert.NoError(t, err)
+    privateKeyStr, publicKeyStr, err := GenerateKey()
+    assert.NoError(t, err)
 
-	t.Logf("TestECC_GenerateKey:  GenerateKey success.")
-	t.Logf("\tPrivateKey:\n%v", privateKeyStr)
-	t.Logf("\tPublicKey:\n%v", publicKeyStr)
+    t.Logf("TestECC_GenerateKey:  GenerateKey success.")
+    t.Logf("\tPrivateKey:\n%v", privateKeyStr)
+    t.Logf("\tPublicKey:\n%v", publicKeyStr)
 }
 
 func TestECC_ParsePrivateKey(t *testing.T) {
-	privateKeyStr, _, _ := GenerateKey()
-	_, err := ParsePrivateKey(privateKeyStr)
-	assert.NoError(t, err)
+    privateKeyStr, _, _ := GenerateKey()
+    _, err := ParsePrivateKey(privateKeyStr)
+    assert.NoError(t, err)
 }
 
 func TestECC_EncryptANDDecrypt(t *testing.T) {
-	privateKeyStr, publicKeyStr, err := GenerateKey()
-	assert.NoError(t, err)
+    plaintext := []byte("s@strluck.com")
 
-	ecdsaPublicKey, err := ParsePublicKey(publicKeyStr)
-	assert.NoError(t, err)
+    ecdsaPublicKey, err := ParsePublicKey(PublicKeyStr)
+    assert.NoError(t, err)
+    ecdsaPrivateKey, err := ParsePrivateKey(PrivateKeyStr)
+    assert.NoError(t, err)
 
-	ecdsaPrivateKey, err := ParsePrivateKey(privateKeyStr)
-	assert.NoError(t, err)
+    ciphertext, err := Encrypt(ecdsaPublicKey, plaintext)
+    assert.NoError(t, err)
+    recoveredPlaintext, err := Decrypt(ecdsaPrivateKey, ciphertext)
+    assert.NoError(t, err)
 
-	plaintext := []byte("s@strluck.com")
-	t.Logf("TestECC_EncryptANDDecrypt:  log:=plaintext: %v", string(plaintext))
+    assert.Equal(t, plaintext, recoveredPlaintext)
 
-	ciphertext, err := Encrypt(ecdsaPublicKey, plaintext)
-	t.Logf("TestECC_EncryptANDDecrypt:  log:=plaintext len: %v", len(ciphertext))
-	assert.NoError(t, err)
-	t.Logf("TestECC_EncryptANDDecrypt:  log:=ciphertext: %v", ciphertext)
+    privateKeyStr, publicKeyStr, err := GenerateKey()
+    assert.NoError(t, err)
+    ecdsaPublicKey, err = ParsePublicKey(publicKeyStr)
+    assert.NoError(t, err)
+    ecdsaPrivateKey, err = ParsePrivateKey(privateKeyStr)
+    assert.NoError(t, err)
 
-	recoveredPlaintext, err := Decrypt(ecdsaPrivateKey, ciphertext)
-	assert.NoError(t, err)
+    ciphertext, err = Encrypt(ecdsaPublicKey, plaintext)
+    assert.NoError(t, err)
+    recoveredPlaintext, err = Decrypt(ecdsaPrivateKey, ciphertext)
+    assert.NoError(t, err)
 
-	assert.Equal(t, plaintext, recoveredPlaintext)
-
-	t.Logf("TestECC_EncryptANDDecrypt:  log:=recoveredPlaintext: %v", string(recoveredPlaintext))
+    assert.Equal(t, plaintext, recoveredPlaintext)
 }
 
 func BenchmarkECC_Encrypt(b *testing.B) {
-	// 停止计时器
-	b.StopTimer()
+    // 停止计时器
+    b.StopTimer()
 
-	_, publicKeyStr, err := GenerateKey()
-	assert.NoError(b, err)
+    _, publicKeyStr, err := GenerateKey()
+    assert.NoError(b, err)
 
-	ecdsaPublicKey, err := ParsePublicKey(publicKeyStr)
-	assert.NoError(b, err)
+    ecdsaPublicKey, err := ParsePublicKey(publicKeyStr)
+    assert.NoError(b, err)
 
-	uuids := make([]string, b.N, b.N)
-	for i := 0; i < b.N; i++ {
-		uuids[i] = uuid.New().String()
-	}
+    uuids := make([]string, b.N, b.N)
+    for i := 0; i < b.N; i++ {
+        uuids[i] = uuid.New().String()
+    }
 
-	// 开始计时器
-	b.StartTimer()
+    // 开始计时器
+    b.StartTimer()
 
-	for i := 0; i < b.N; i++ {
-		_, err := Encrypt(ecdsaPublicKey, []byte(uuids[i]))
-		if err != nil {
-			b.Errorf("BenchmarkECC_Encrypt:  Encrypt failed!err:=%v", err)
-		}
-	}
+    for i := 0; i < b.N; i++ {
+        _, err := Encrypt(ecdsaPublicKey, []byte(uuids[i]))
+        if err != nil {
+            b.Errorf("BenchmarkECC_Encrypt:  Encrypt failed!err:=%v", err)
+        }
+    }
 }
 
 func BenchmarkECC_Decrypt(b *testing.B) {
-	b.StopTimer()
+    b.StopTimer()
 
-	privateKeyStr, publicKeyStr, err := GenerateKey()
-	assert.NoError(b, err)
+    privateKeyStr, publicKeyStr, err := GenerateKey()
+    assert.NoError(b, err)
 
-	ecdsaPublicKey, err := ParsePublicKey(publicKeyStr)
-	assert.NoError(b, err)
+    ecdsaPublicKey, err := ParsePublicKey(publicKeyStr)
+    assert.NoError(b, err)
 
-	ecdsaPrivateKey, err := ParsePrivateKey(privateKeyStr)
-	assert.NoError(b, err)
+    ecdsaPrivateKey, err := ParsePrivateKey(privateKeyStr)
+    assert.NoError(b, err)
 
-	ciphertexts := make([]string, b.N, b.N)
+    ciphertexts := make([]string, b.N, b.N)
 
-	for i := 0; i < b.N; i++ {
-		ciphertext, err := Encrypt(ecdsaPublicKey, []byte(uuid.New().String()))
-		assert.NoError(b, err)
-		ciphertexts[i] = ciphertext
-	}
+    for i := 0; i < b.N; i++ {
+        ciphertext, err := Encrypt(ecdsaPublicKey, []byte(uuid.New().String()))
+        assert.NoError(b, err)
+        ciphertexts[i] = ciphertext
+    }
 
-	b.StartTimer()
+    b.StartTimer()
 
-	for i := 0; i < b.N; i++ {
-		_, err := Decrypt(ecdsaPrivateKey, ciphertexts[i])
-		if err != nil {
-			b.Errorf("BenchmarkECC_Decrypt:  Decrypt failed!err:=%v", err)
-		}
-	}
+    for i := 0; i < b.N; i++ {
+        _, err := Decrypt(ecdsaPrivateKey, ciphertexts[i])
+        if err != nil {
+            b.Errorf("BenchmarkECC_Decrypt:  Decrypt failed!err:=%v", err)
+        }
+    }
 }
