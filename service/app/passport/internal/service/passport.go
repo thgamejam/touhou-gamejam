57088185a656c574f9b66038fa7d87189626d304
null122 <1229608477@qq.com>
2022-05-23 21:51:30 +0800

    feat(passport): 预创建用户

--- service/app/passport/internal/service/passport.go
+++ service/app/passport/internal/service/passport.go
@@ -2,40 +2,34 @@ package service
 
 import (
 	"context"
-	"github.com/go-kratos/kratos/v2/log"
-	pb "service/api/passport/v1"
 	"service/app/passport/internal/biz"
+
+	pb "service/api/passport/v1"
 )
 
-type PassportService struct {
-	pb.UnimplementedPassportServer
-
-	uc  *biz.PassportUseCase
-	log *log.Helper
-}
-
-func NewPassportService(
-	uc *biz.PassportUseCase,
-	logger log.Logger,
-) *PassportService {
-	return &PassportService{
-		uc:  uc,
-		log: log.NewHelper(logger),
+// CreateAccount 预创建账户
+func (s *PassportService) CreateAccount(ctx context.Context, req *pb.CreateAccountRequest) (*pb.CreateAccountReply, error) {
+	err := s.uc.PrepareCreateAccount(ctx, biz.Account{
+		Email:    req.Body.Email,
+		Password: req.Body.Password,
+		Hash:     req.Body.Hash,
+	}, req.Body.Token)
+	if err != nil {
+		return nil, err
 	}
+	return &pb.CreateAccountReply{
+		Ok: true,
+	}, nil
 }
-
-func (s *PassportService) CreatePassport(ctx context.Context, req *pb.CreatePassportRequest) (*pb.CreatePassportReply, error) {
-	return &pb.CreatePassportReply{}, nil
+func (s *PassportService) VerifyEmail(ctx context.Context, req *pb.VerifyEmailRequest) (*pb.VerifyEmailReply, error) {
+	return &pb.VerifyEmailReply{}, nil
 }
-func (s *PassportService) DeletePassport(ctx context.Context, req *pb.DeletePassportRequest) (*pb.DeletePassportReply, error) {
-	return &pb.DeletePassportReply{}, nil
+func (s *PassportService) GetPublicKey(ctx context.Context, req *pb.GetPublicKeyRequest) (*pb.GetPublicKeyReply, error) {
+	return &pb.GetPublicKeyReply{}, nil
 }
-func (s *PassportService) GetPassport(ctx context.Context, req *pb.GetPassportRequest) (*pb.GetPassportReply, error) {
-	return &pb.GetPassportReply{}, nil
+func (s *PassportService) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginReply, error) {
+	return &pb.LoginReply{}, nil
 }
-func (s *PassportService) UpdatePassport(ctx context.Context, req *pb.UpdatePassportRequest) (*pb.UpdatePassportReply, error) {
-	return &pb.UpdatePassportReply{}, nil
-}
-func (s *PassportService) ListPassport(ctx context.Context, req *pb.ListPassportRequest) (*pb.ListPassportReply, error) {
-	return &pb.ListPassportReply{}, nil
+func (s *PassportService) ChangePassword(ctx context.Context, req *pb.ChangePasswordRequest) (*pb.ChangePasswordReply, error) {
+	return &pb.ChangePasswordReply{}, nil
 }
