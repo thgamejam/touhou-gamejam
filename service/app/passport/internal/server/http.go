ae4836db51fb3ab3bcdbff95168b89291d21ae25
null122 <1229608477@qq.com>
2022-06-02 23:46:32 +0800

    feat(passport): 完善jwt签名认证

--- service/app/passport/internal/server/http.go
+++ service/app/passport/internal/server/http.go
@@ -3,21 +3,23 @@ package server
 import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	v1 "service/api/passport/v1"
-	//"service/app/passport/internal/conf"
+	"service/app/passport/internal/conf"
 	"service/app/passport/internal/service"
-
 	pkgConf "service/pkg/conf"
+	"service/pkg/jwt"
 )
 
 // NewHTTPServer new a HTTP server.
-func NewHTTPServer(c *pkgConf.Server, service *service.PassportService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *pkgConf.Server, t *conf.Passport, service *service.PassportService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
 			validate.Validator(),
+			selector.Server(jwt.JWTLoginAuth([]byte(t.VerifyEmailKey))).Match(jwt.ValidateLoginListMatcher()).Build(),
 		),
 	}
 	if c.Http.Network != "" {
