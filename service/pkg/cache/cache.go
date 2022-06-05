8fbafba1555f066a798a06304bfda1868d1363f5
Strluck <s@strluck.com>
2022-06-05 20:32:49 +0800

    feat(pkg/cache): 对外暴露redis.Client

--- service/pkg/cache/cache.go
+++ service/pkg/cache/cache.go
@@ -1,89 +1,90 @@
 package cache
 
 import (
-    "context"
-    "encoding/json"
-    "github.com/go-redis/redis/v8"
-    "service/pkg/conf"
-    "time"
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"service/pkg/conf"
+	"time"
 )
 
 type Cache struct {
-    client *redis.Client // redis客户端
+	Client *redis.Client // redis客户端
 
-    DelayDeleteTime time.Duration // 延迟删除等待时间
+	DelayDeleteTime time.Duration // 延迟删除等待时间
 }
 
 // NewCache 初始化缓存
 func NewCache(c *conf.Service) (*Cache, error) {
-    rdb := redis.NewClient(&redis.Options{
-        Network:      c.Data.Redis.Network,
-        Addr:         c.Data.Redis.Addr,
-        Password:     c.Data.Redis.Password, // no password set
-        ReadTimeout:  c.Data.Redis.ReadTimeout.AsDuration(),
-        WriteTimeout: c.Data.Redis.WriteTimeout.AsDuration(),
-        DB:           0, // use default DB
-    })
+	rdb := redis.NewClient(&redis.Options{
+		Network:      c.Data.Redis.Network,
+		Addr:         c.Data.Redis.Addr,
+		Password:     c.Data.Redis.Password, // no password set
+		ReadTimeout:  c.Data.Redis.ReadTimeout.AsDuration(),
+		WriteTimeout: c.Data.Redis.WriteTimeout.AsDuration(),
+		DB:           0, // use default DB
+	})
 
-    return &Cache{
-        client:          rdb,
-        DelayDeleteTime: c.Data.Redis.DelayDeleteTime.AsDuration(),
-    }, nil
+	return &Cache{
+		Client:          rdb,
+		DelayDeleteTime: c.Data.Redis.DelayDeleteTime.AsDuration(),
+	}, nil
 }
 
 // Set 保存数据
-func (c *Cache) Set(ctx context.Context, key string, data interface{}, expiration time.Duration) error {
-    jsonByte, err := json.Marshal(data)
-    if err != nil {
-        return err
-    }
-    jsonString := string(jsonByte)
-    return c.client.Set(ctx, key, jsonString, expiration).Err()
+func (c *Cache) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	jsonByte, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	jsonString := string(jsonByte)
+	return c.Client.Set(ctx, key, jsonString, expiration).Err()
 }
 
 func (c *Cache) SetString(ctx context.Context, key string, value string, expiration time.Duration) error {
-    return c.client.Set(ctx, key, value, expiration).Err()
+	return c.Client.Set(ctx, key, value, expiration).Err()
 }
 
 // Get 获取数据
-func (c *Cache) Get(ctx context.Context, key string, v interface{}) (ok bool, err error) {
-    value, err := c.client.Get(ctx, key).Result()
-    if err == redis.Nil {
-        return false, nil
-    } else if err != nil {
-        return
-    }
-    return true, json.Unmarshal([]byte(value), v)
+// value 必须是一个指针
+func (c *Cache) Get(ctx context.Context, key string, value interface{}) (ok bool, err error) {
+	v, err := c.Client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return false, nil
+	} else if err != nil {
+		return
+	}
+	return true, json.Unmarshal([]byte(v), value)
 }
 
 func (c *Cache) GetString(ctx context.Context, key string) (value string, ok bool, err error) {
-    value, err = c.client.Get(ctx, key).Result()
-    if err == redis.Nil {
-        return value, false, nil
-    } else if err != nil {
-        return value, false, err
-    }
-    return value, true, nil
+	value, err = c.Client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return value, false, nil
+	} else if err != nil {
+		return value, false, err
+	}
+	return value, true, nil
 }
 
 // Del 删除数据
 func (c *Cache) Del(ctx context.Context, key string) (err error) {
-    _, err = c.client.Del(ctx, key).Result()
+	_, err = c.Client.Del(ctx, key).Result()
 
-    // TODO 简单的延迟删除
-    var delayDel = func(key string) {
-        time.Sleep(c.DelayDeleteTime)
-        _, _ = c.client.Del(context.Background(), key).Result()
-    }
-    go delayDel(key)
+	// TODO 简单的延迟删除
+	var delayDel = func(key string) {
+		time.Sleep(c.DelayDeleteTime)
+		_, _ = c.Client.Del(context.Background(), key).Result()
+	}
+	go delayDel(key)
 
-    return err
+	return err
 }
 
 func (c *Cache) Close() error {
-    return c.client.Close()
+	return c.Client.Close()
 }
 
 func (c *Cache) GetClient() *redis.Client {
-    return c.client
+	return c.Client
 }
